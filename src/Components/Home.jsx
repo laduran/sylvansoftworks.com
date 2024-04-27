568f10e95ff71038bab9d45fc73ede77c2e9109c
Louis <louis.duran@gmail.com>
2024-04-26 22:03:22 -0700

    title looks a bit better in mobile. change footer color

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,7 @@ import PropTypes from "prop-types";
  * freely use on your site.
  */
 import image from "../images/foggy_lake.jpg";
-const imageAltText = "Python Code Image";
+const imageAltText = "Foggy Lake";
 
 const Home = ({ name, title }) => {
   return (
@@ -29,7 +29,7 @@ const Home = ({ name, title }) => {
       <img className="background" src={image} alt={imageAltText} />
       <div style={{ position: "absolute",
                     top: "5rem", 
-                    left: "2rem", 
+                    left: "1rem", 
                     width: "17rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
